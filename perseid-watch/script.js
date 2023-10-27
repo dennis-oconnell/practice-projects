9aadfc1448bc1ac7e6ede0c76f4e89abe1658c59
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-10-27 14:10:11 -0700

    initial commit with base template and initial commit of Perseid Watch
    initial commit with base template and initial commit of Perseid Watch

---
+++ perseid-watch/script.js
@@ -0,0 +1,5 @@
+// JavaScript code for your project goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Your code that interacts with the DOM or performs other actions goes here
+});
