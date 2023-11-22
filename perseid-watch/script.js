ad42695081392f2a2d73bd6650451e567ffed719
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-11-21 21:01:20 -0800

    using javascript to update html elements
    using javascript to update html elements

--- perseid-watch/script.js
+++ perseid-watch/script.js
@@ -1,6 +1,14 @@
-let currentDate = new Date();
 let perseidDate = new Date("Aug 12, 2024 00:20:00");
 
-let timeToPerseid = perseidDate - currentDate;
+function updateTimeLeft() {
+	let currentDate = new Date();
+	let milisecondsLeft = perseidDate - currentDate;
+	let daysLeft = Math.floor(milisecondsLeft / (3600 * 1000) / 24);
+	let hoursLeft = Math.floor(milisecondsLeft / (3600 * 1000) - daysLeft * 24);
+	let minsLeft = Math.floor(milisecondsLeft / (3600 * 1000) - daysLeft * 24);
 
-console.log(timeToPerseid);
+	document.getElementById("days").innerText = daysLeft;
+	document.getElementById("hours").innerText = hoursLeft;
+}
+
+setInterval(updateTimeLeft, 1000);
