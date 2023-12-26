75570687e3581de8f86efb87941bfe56b2699b74
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-12-26 09:33:55 -0800

    Implemented Error Handling

--- golang_docs_tutorial_projects/hello_modules/greetings/greetings.go
+++ golang_docs_tutorial_projects/hello_modules/greetings/greetings.go
@@ -1,12 +1,20 @@
 // Declare a greetings package to collect related functions.
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person. Implement a Hello function to return the greeting.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
 
 //In Go, the := operator is a shortcut for declaring and initializing a variable in one line (Go uses the value on the right to determine the variable's type). Taking the long way, you might have written this as:
