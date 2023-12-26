c91f9fe6e5e55f2d01823620c39ee97615674dd0
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-12-25 18:24:33 -0800

    Called code in an external quote package

--- golang_tutorial/hello_world.go
+++ golang_tutorial/hello_world.go
@@ -2,9 +2,19 @@
 package main
 
 //Import the popular fmt package, which contains functions for formatting text, including printing to the console. This package is one of the standard library packages you got when you installed Go.
-import "fmt"
+import (
+	"fmt"
 
-//Implement a main function to print a message to the console. A main function executes by default when you run the main package.
+	"rsc.io/quote"
+)
+
+//Implement a main function to print a message to the console. A main function executes by default when you run the main package.g
 func main() {
-	fmt.Println("This is my Go Hello World Program! It's so Hello Worldy!")
+	fmt.Println("This is my Go Hello World Program; it's so Hello Worldy! On the next line, look for a pithy quote!")
+
+	fmt.Println(quote.Go())
+	fmt.Println("And on the next few lines, we'll call some other functions from the quote package for more interesting and useful quotes!")
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }
