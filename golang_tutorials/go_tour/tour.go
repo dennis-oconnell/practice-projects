05eed07af7d1002f63f81fbfc3cb0891540f5f52
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-12-26 10:24:57 -0800

    completed up to step 7 of tour of go

--- golang_tutorials/go_tour/tour.go
+++ golang_tutorials/go_tour/tour.go
@@ -6,15 +6,25 @@ import (
 	"math/rand"
 )
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x 
+	return 
 }
 
 func main() {
 	fmt.Println("1. My favorite number is", rand.Intn(10))
 
-	//Printf is print formatted
-	//%g is a format specifier used for floating point numbers
+	
+	//Printf stands for print formatted. %g is a format specifier used for floating point numbers
 	fmt.Printf("2. Now you have %g problems. \n", math.Sqrt(7))
 
 	//In Go, a name is exported if it begins with a capital letter. 
@@ -24,4 +34,18 @@ func main() {
 	//A function can take zero or more arguments.
 	//Notice that the type comes after the variable name.
 	fmt.Println("4. Two plus four is equal to", add(2,4))
+
+	//A function can return any number of results. The swap function returns two strings.
+	a, b := swap("hello", "world")
+	fmt.Println("5. If I swap the words 'hello' and 'world' I get the following:",a, b)
+
+	//Step 6 was a matter of reformatting the add function
+	fmt.Println("6. Step 6 was a matter of reformatting previous work")
+
+	//Go's return values may be named. If so, they are treated as variables defined at the top of the function
+	//A return statement without arguments returns the named return values. This is known as a "naked" return.
+	//Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions.
+	fmt.Print("7. If I wanted to split a number in two such as the number 17, it would look like:")
+	fmt.Print(split(17))
+
 }
