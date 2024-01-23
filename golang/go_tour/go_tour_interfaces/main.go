f6fbc969a68783527413f53c0582e051efe028d1
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-01-22 22:16:35 -0800

    seperated go methods tour and go interfaces tour, began exercise 10

---
+++ golang/go_tour/go_tour_interfaces/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//9. Interfaces
+type Abser interface {
+	Abs() float64
+}
+
+//10. Interfaces are implemented implicitly
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	//9. Interfaces
+	//An Interface type is defined as a set of method signatures
+	//A value interface type can hold any value that implements those methods
+	var abe Abser
+	h := MyFloat(-math.Sqrt2)
+	g := Vertex{3,4}
+
+	abe = h		//a MyFloat implements Abser
+	abe = &g		//a *Vertex implements Abser
+
+	fmt.Println(abe.Abs())
+
+	//10. Interfaces are implemented implicitly
+	//A type implements an interface by implementing its methods
+	//There is no explicit declaration of intent (no 'implements' keyword)
+	//Implicit interfaces decouple the definition of an interface from its implementation, which could then appear in any package without prearrangement
+	var i I = T{"hello"}
+	i.M()
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
--- golang/go_tour/go_tour_methods/main.go
+++ golang/go_tour/go_tour_methods/main.go
@@ -67,18 +67,6 @@ func AbsFunc(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-//9. My First Interface!
-type Abser interface {
-	Abs() float64
-}
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
 func main() {
 	//1. Methods
 	//Go has no classes! Java programmers be warned!
@@ -183,17 +171,4 @@ func main() {
 			//This can be more efficient if the receiver is a large struct, for example
 	
 	
-
-
-	//9. Interfaces
-	//An Interface type is defined as a set of method signatures
-	//A value interface type can hold any value that implements those methods
-	var abe Abser
-	h := MyFloat(-math.Sqrt2)
-	g := Vertex{3,4}
-
-	abe = h		//a MyFloat implements Abser
-	abe = &g		//a *Vertex implements Abser
-
-	fmt.Println(abe.Abs())
 }
