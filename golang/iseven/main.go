0eb50d7b9a5ace2f79c0e5bb29bea4de8020a29f
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-02-20 20:04:26 -0800

    used append to append to slices

--- golang/iseven/main.go
+++ golang/iseven/main.go
@@ -13,10 +13,9 @@ the all even numbers in the list.
 func IsEven(input []int) []int{
 	output:= []int{}
 
-	for x := 0; x < len(input); x++ { // ++ is a statement.
-        fmt.Println(input[x])
+	for x := 0; x < len(input); x++ {
 		if input[x] % 2 == 0 {
-			output[x] = input[x]
+			output = append(output, input[x])
 		}
     }
 
@@ -26,12 +25,9 @@ func IsEven(input []int) []int{
 func main() {
 	testnums := []int{1,2,3,4,5,6,7,8,9,10}
 
-	for x := 0; x < len(testnums); x++ { // ++ is a statement.
-        if testnums[x] % 2 == 0 {
-			fmt.Println(testnums[x])
-		}
-		
-    }
+	fmt.Println(IsEven(testnums))
 
-	IsEven(testnums)
+	testmor := []int{1,6,23,2,1,64,-1,2,4,2}
+
+	fmt.Println(IsEven(testmor))
 }
