4182d4675130bf858c9712a8d2687d3ac71f55c2
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-02-24 07:54:29 -0800

    created new variables for author, special offer, and discount

--- golang/fortheloveofgo/happyfunbooks/main.go
+++ golang/fortheloveofgo/happyfunbooks/main.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 	var title string
 	var copies int
+	var author string
+	var specialoffer bool
+	var discount float64
 	title = "For The Love of Go"
+	author = "John Arundel"
+	specialoffer = true
+	discount = .9
 	copies = 99
 	fmt.Println(title)
+	fmt.Println(author)
+	fmt.Println(specialoffer)
+	fmt.Println(discount)
 	fmt.Println(copies)
 }
