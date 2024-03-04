834dfc5a7842a43664056198058b5584f0860e8b
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-03-03 20:50:28 -0800

    setup enviornment for The Go Programming Language Book Exercises

---
+++ golang/thegoprogramminglanguage/chp1exercises/ex1/main.go
@@ -0,0 +1,15 @@
+// TITLE: GOPL Chapter 1 Exercise 1
+// DATE: 03/03/2024
+// START: 8:50
+// END:
+// MINUTES:
+// ACCURACY:
+// GRADE:
+
+// PROBLEM: Exercise 1.1: Modify the echo program to also print os.Args[0], the name of the command that invoked it.
+
+package main
+
+func main(){
+
+}
