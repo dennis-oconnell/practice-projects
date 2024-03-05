15ac33e0bf8a5860684dd66e05dae7f6b12fd044
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-03-04 20:54:56 -0800

    completed exercise 1.1

--- golang/thegoprogramminglanguage/chp1exercises/ex1/main.go
+++ golang/thegoprogramminglanguage/chp1exercises/ex1/main.go
@@ -1,15 +1,20 @@
 // TITLE: GOPL Chapter 1 Exercise 1
-// DATE: 03/03/2024
-// START: 8:50
-// END:
-// MINUTES:
-// ACCURACY:
-// GRADE:
+// DATE: 03/04/2024
+// START: 8:49
+// END: 8:51
+// MINUTES: 2
+// GRADE: A
 
 // PROBLEM: Exercise 1.1: Modify the echo program to also print os.Args[0], the name of the command that invoked it.
-
+// SOLUTION: When os.Args[0] is printed the result is the name of the command that invoked it
 package main
 
-func main(){
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main(){
+	fmt.Println(strings.Join(os.Args[0:], " "))	
 }
