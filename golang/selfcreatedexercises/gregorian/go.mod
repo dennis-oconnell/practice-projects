9d57d5494203f01a63d3f364e53621be8f2cd435
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2024-04-12 13:59:13 -0700

    created gregorian go module project

---
+++ golang/selfcreatedexercises/gregorian/go.mod
@@ -0,0 +1,3 @@
+module github.com/dennis-oconnell/go-gregorian
+
+go 1.21.5
---
+++ golang/selfcreatedexercises/gregorian/gregorian_test.go
@@ -0,0 +1,11 @@
+package gregorian_test
+
+import (
+	"testing"
+)
+
+func Test_Todays_Gregorian_Date(t *testing.T) {
+	t.Parallel()
+
+
+}
